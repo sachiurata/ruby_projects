5e456d17ee81e3a54a9b60fc9f32c352463d21a6
sachi <urata986@gmail.com>
2023-03-06 03:26:23 +0000

    third message

--- hello.rb
+++ hello.rb
@@ -1,20 +1,44 @@
 class People
-  def initialize
+  attr_accessor :name
+  def initialize(name)
+    self.name = name
    puts "Peopleのインスタンスが作られました"
   end
-  def self.introduce1
+  def self.introduce
     puts "私はPeopleクラスです"
   end
+  def say
+    puts "私の名前は#{self.name}です"
+  end
   
-  attr_accessor :name
+  def talk(other)
+    puts "#{self.name}が#{other.name}に話しかける"
+  end
 
 end
 
 class ChildPeople < People
-  def self.introduce2
-    puts "私は目からビームが出せます"
+  attr_accessor :age
+  def initialize(name,age)
+    #self.name = name
+    super(name)
+    self.age = age
+  end
+  def introduce
+    puts "私の名前は#{self.name}、年齢は#{self.age}です"
   end
 end
 
-People.introduce1
-ChildPeople.introduce2
+People.introduce
+ChildPeople.introduce
+sachi = People.new("さち")
+sachi.say
+shima = People.new("しま")
+puts shima.name
+shima.say
+sachi.talk(shima)
+shima.talk(sachi)
+#people.name = "さち"
+#p shima
+tanaka = ChildPeople.new("田中", 20)
+tanaka.introduce
