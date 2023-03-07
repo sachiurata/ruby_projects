9c34a4aa69df896208950af56146c0ed46dca3e9
sachi <urata986@gmail.com>
2023-03-07 06:22:00 +0000

    fifth message

--- hello.rb
+++ hello.rb
@@ -1,49 +1,3 @@
-class People
-  attr_accessor :name
-  def initialize(name)
-    self.name = name
-   puts "Peopleのインスタンスが作られました"
-  end
-  def self.introduce
-    puts "私はPeopleクラスです"
-  end
-  def say
-    puts "私の名前は#{self.name}です"
-  end
-  
-  def talk(other)
-    puts "#{self.name}が#{other.name}に話しかける"
-  end
-
-end
-
-class ChildPeople < People
-  attr_accessor :age
-  def initialize(name,age)
-    #self.name = name
-    super(name)
-    self.age = age
-  end
-  def introduce
-    puts "私の名前は#{self.name}、年齢は#{self.age}です"
-  end
-end
-
-People.introduce
-ChildPeople.introduce
-sachi = People.new("さち")
-sachi.say
-shima = People.new("しま")
-puts shima.name
-shima.say
-sachi.talk(shima)
-shima.talk(sachi)
-#people.name = "さち"
-#p shima
-tanaka = ChildPeople.new("田中", 20)
-tanaka.introduce
-
-
 #年齢
 class Age
   attr_accessor :name
@@ -150,9 +104,56 @@ class Bmi
   
 end
 
-yamada = Bmi.new("山田", "鈴木", 55, 1.6)
+yamada = Bmi.new("山田", "鈴木", 80, 1.6)
 yamada.bmi
 yamada.say
 suzuki = Bmi.new("鈴木", "田中", 45, 1.55)
 suzuki.bmi
 suzuki.say
+
+
+#メンタリング3/6
+class People
+  attr_accessor :name
+  def initialize(name)
+    self.name = name
+   puts "Peopleのインスタンスが作られました"
+  end
+  def self.introduce
+    puts "私はPeopleクラスです"
+  end
+  def say
+    puts "私の名前は#{self.name}です"
+  end
+  
+  def talk(other)
+    puts "#{self.name}が#{other.name}に話しかける"
+  end
+
+end
+
+class ChildPeople < People
+  attr_accessor :age
+  def initialize(name,age)
+    #self.name = name
+    super(name)
+    self.age = age
+  end
+  def introduce
+    puts "私の名前は#{self.name}、年齢は#{self.age}です"
+  end
+end
+
+People.introduce
+ChildPeople.introduce
+sachi = People.new("さち")
+sachi.say
+shima = People.new("しま")
+puts shima.name
+shima.say
+sachi.talk(shima)
+shima.talk(sachi)
+#people.name = "さち"
+#p shima
+tanaka = ChildPeople.new("田中", 20)
+tanaka.introduce
